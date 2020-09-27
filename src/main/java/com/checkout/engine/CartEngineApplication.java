de35c4e6f0fb3357c9783ce31ead268c47efe031
Sushil Yadav <sushil.yadav@ncs.com.sg>
2020-09-27 14:49:25 +0530

    springboot basic project added

---
+++ src/main/java/com/checkout/engine/CartEngineApplication.java
@@ -0,0 +1,13 @@
+package com.checkout.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkout/engine/CartEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkout.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
