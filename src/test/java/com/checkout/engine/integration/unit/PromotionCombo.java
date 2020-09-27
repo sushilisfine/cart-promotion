217f9e27f1ec532d8c97ef42b1f70d016c08fd1a
Sushil Yadav <sushil.yadav@ncs.com.sg>
2020-09-27 15:41:52 +0530

    unit test logic for promotion applied on SKU A

---
+++ src/test/java/com/checkout/engine/integration/unit/PromotionCombo.java
@@ -0,0 +1,56 @@
+package com.checkout.engine.integration.unit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.checkout.engine.entities.dto.CartDTO;
+import com.checkout.engine.promotion.types.Promotion2BCombo;
+import com.checkout.engine.promotion.types.Promotion3ACombo;
+import com.checkout.engine.promotion.types.PromotionCDCombo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PromotionCombo {
+
+	@Autowired
+	private Promotion3ACombo promotion3ACombo;
+
+	private CartDTO cartObj0() {
+		CartDTO cart = new CartDTO();
+		cart.setPromotionPackage("P1");
+		return cart;
+	}
+
+	private CartDTO cartObj1() {
+		CartDTO cart = new CartDTO();
+		cart.setSkuA(1);
+		cart.setSkuB(1);
+		cart.setSkuC(1);
+		cart.setPromotionPackage("P1");
+
+		return cart;
+	}
+
+	
+	@Test
+	public void test0Promotio3A() {
+		CartDTO cartObj = cartObj0();
+		assertThat(promotion3ACombo.apply(cartObj)).isEqualTo(0.0);
+
+	}
+
+	@Test
+	public void test1Promotion3A() {
+		CartDTO cartObj = cartObj1();
+		assertThat(promotion3ACombo.apply(cartObj)).isEqualTo(0.0);
+
+	}
+}
