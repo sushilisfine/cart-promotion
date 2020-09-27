d2d51a3c2acec3354afefde9110e9d1821dd2e46
Sushil Yadav <sushil.yadav@ncs.com.sg>
2020-09-27 16:36:41 +0530

    service layer logic for cart promotion calculation

--- src/main/java/com/checkout/engine/service/PromotionService.java
+++ src/main/java/com/checkout/engine/service/PromotionService.java
@@ -1,14 +1,29 @@
 package com.checkout.engine.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.checkout.engine.entities.dto.CartDTO;
+import com.checkout.engine.promotion.category.PromotionCategory;
+import com.checkout.engine.promotion.category.PromotionFactory;
 
 @Service
 public class PromotionService {
 
+	@Autowired
+	PromotionFactory promotionFactory;
+
 	public Double calculateTotal(CartDTO cart) {
-		return 0.0;
+
+		String promotionPackage = cart.getPromotionPackage();
+
+		// default promotion if none selected from clients end
+		if (promotionPackage == null)
+			promotionPackage = "P1";
+
+		PromotionCategory promotionCategory = promotionFactory.getPromotionCategory(cart.getPromotionPackage());
+
+		return promotionCategory.applyPromotions(cart);
 	}
 
 }
