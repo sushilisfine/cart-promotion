e85000067f51c900148854ad2a2522db9287b3c8
Sushil Yadav <sushil.yadav@ncs.com.sg>
2020-09-27 16:47:48 +0530

    integration tests with different values added

--- src/test/java/com/checkout/engine/integration/PromotionControllerTest.java
+++ src/test/java/com/checkout/engine/integration/PromotionControllerTest.java
@@ -23,6 +23,39 @@ public class PromotionControllerTest extends CartEngineApplicationTests {
 
 	private CartDTO cartObj0() {
 		CartDTO cart = new CartDTO();
+		cart.setPromotionPackage("P1");
+
+		return cart;
+	}
+
+	private CartDTO cartObj1() {
+		CartDTO cart = new CartDTO();
+		cart.setSkuA(1);
+		cart.setSkuB(1);
+		cart.setSkuC(1);
+		cart.setPromotionPackage("P1");
+
+		return cart;
+	}
+
+	private CartDTO cartObj2() {
+		CartDTO cart = new CartDTO();
+		cart.setSkuA(5);
+		cart.setSkuB(5);
+		cart.setSkuC(1);
+		cart.setPromotionPackage("P1");
+
+		return cart;
+	}
+
+	private CartDTO cartObj3() {
+		CartDTO cart = new CartDTO();
+		cart.setSkuA(3);
+		cart.setSkuB(5);
+		cart.setSkuC(1);
+		cart.setSkuD(1);
+		cart.setPromotionPackage("P1");
+
 		return cart;
 	}
 
@@ -42,6 +75,63 @@ public class PromotionControllerTest extends CartEngineApplicationTests {
 		System.out.println(content);
 
 		assertEquals(Double.parseDouble(content), 0.0, 0);
+
+	}
+
+	@Test
+	public void test1Checkout() throws Exception {
+		String uri = "/api/v1/checkout";
+
+		CartDTO cartObj = cartObj1();
+
+		String inputJson = super.mapToJson(cartObj);
+		MvcResult mvcResult = mvc.perform(
+				MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
+				.andReturn();
+		int httpStatusCode = mvcResult.getResponse().getStatus();
+		assertEquals(httpStatusCode, 200);
+		String content = mvcResult.getResponse().getContentAsString();
+		System.out.println(content);
+
+		assertEquals(Double.parseDouble(content), 100.0, 0);
+
+	}
+
+	@Test
+	public void test2Checkout() throws Exception {
+		String uri = "/api/v1/checkout";
+
+		CartDTO cartObj = cartObj2();
+
+		String inputJson = super.mapToJson(cartObj);
+		MvcResult mvcResult = mvc.perform(
+				MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
+				.andReturn();
+		int httpStatusCode = mvcResult.getResponse().getStatus();
+		assertEquals(httpStatusCode, 200);
+		String content = mvcResult.getResponse().getContentAsString();
+		System.out.println(content);
+
+		assertEquals(Double.parseDouble(content), 370.0, 0);
+
+	}
+
+	@Test
+	public void test3Checkout() throws Exception {
+		String uri = "/api/v1/checkout";
+
+		CartDTO cartObj = cartObj3();
+
+		String inputJson = super.mapToJson(cartObj);
+		MvcResult mvcResult = mvc.perform(
+				MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
+				.andReturn();
+		int httpStatusCode = mvcResult.getResponse().getStatus();
+		assertEquals(httpStatusCode, 200);
+		String content = mvcResult.getResponse().getContentAsString();
+		System.out.println(content);
+
+		assertEquals(Double.parseDouble(content), 280.0, 0);
 
 	}
 }
