bcc1242acae1ad4c50430f0db9740ebb21b041e6
Sushil Yadav <sushil.yadav@ncs.com.sg>
2020-09-27 16:12:46 +0530

    category P1 total calculation

--- src/main/java/com/checkout/engine/promotion/category/PromotionCategoryP1.java
+++ src/main/java/com/checkout/engine/promotion/category/PromotionCategoryP1.java
@@ -6,10 +6,19 @@ import com.checkout.engine.promotion.types.Promotion3ACombo;
 import com.checkout.engine.promotion.types.PromotionCDCombo;
 
 public class PromotionCategoryP1 implements PromotionCategory {
+	
+	Promotion3ACombo promotion3ACombo = new Promotion3ACombo();
+	Promotion2BCombo promotion2BCombo = new Promotion2BCombo();
+	PromotionCDCombo promotionCDCombo = new PromotionCDCombo();
 
 	public Double applyPromotions(CartDTO cart) {
 
-		return 0.0;
-	}
+		Double total = 0.0;
+		total += promotion3ACombo.apply(cart);
+		total += promotion2BCombo.apply(cart);
+		total += promotionCDCombo.apply(cart);
 
-}
+		total += leftOverSkuTotal(cart);
+
+		return total;
+	}}
