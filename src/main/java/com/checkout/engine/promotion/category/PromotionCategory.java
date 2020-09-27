7beb3c953719ded83ceb3e67e8b1efb7d176774b
Sushil Yadav <sushil.yadav@ncs.com.sg>
2020-09-27 16:21:48 +0530

    total calculation of SKU's left after promotion applied

--- src/main/java/com/checkout/engine/promotion/category/PromotionCategory.java
+++ src/main/java/com/checkout/engine/promotion/category/PromotionCategory.java
@@ -1,12 +1,33 @@
 package com.checkout.engine.promotion.category;
 
 import com.checkout.engine.entities.dto.CartDTO;
+import com.checkout.engine.sku.SKU_A;
+import com.checkout.engine.sku.SKU_B;
+import com.checkout.engine.sku.SKU_C;
+import com.checkout.engine.sku.SKU_D;
 
 public interface PromotionCategory {
 
+	public static final SKU_A skuA = new SKU_A();
+
+	public static final SKU_B skuB = new SKU_B();
+
+	public static final SKU_C skuC = new SKU_C();
+
+	public static final SKU_D skuD = new SKU_D();
+
 	default Double leftOverSkuTotal(CartDTO cart) {
-		return 0.0;
+
+		double total = 0.0;
+
+		total += cart.getSkuA() * skuA.getPrice();
+		total += cart.getSkuB() * skuB.getPrice();
+		total += cart.getSkuC() * skuC.getPrice();
+		total += cart.getSkuD() * skuD.getPrice();
+
+		return total;
 	}
 
 	public Double applyPromotions(CartDTO cart);
+
 }
